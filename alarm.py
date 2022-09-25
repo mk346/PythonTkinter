88e763f8d3c4d2e11249696956c005c34fd8a0e5
mk346 <calebkinyuamunene@gmail.com>
2022-09-25 13:23:56 +0300

    update

--- alarm.py
+++ alarm.py
@@ -8,7 +8,7 @@ alarm_date = input('Enter the date on which you want to set the alarm: ').strip(
 alarm_time = ''.join(input("Enter the time of alarm to be set in HH:MM,AM/PM format: ").split())
 music_or_beep = input("Enter m for a music or b for a beep sound:")
 if music_or_beep == 'b':
-    dur = int(input("duration in seconds:"))*1000 # winsound takes in milliseconds
+    dur = int(input("duration in seconds: "))*1000 # winsound takes in milliseconds
     freq = int(input("Frequency of the noise: ")) # optimal- 500
 alarm_hour = alarm_time[0:2]
 alarm_minute = alarm_time[3:5]
